66ad39b55c85fe622b4e6bdb773f50add90e3bc2
PaoloBonicco7 <44091199+PaoloBonicco7@users.noreply.github.com>
2019-05-14 16:31:23 +0200

    Funziona lo scambio BASILARE di messaggi tra server e client, ora il server dopo aver ricevuto un messaggio con azione "SEND" lo rimanda al client e lo visualizza nella textbox. Il mio l'ho fatto chiama quando vuoi andare alla discussione

--- ProgettoPaoloFedeClient/src/progettopaolofede/Client.java
+++ ProgettoPaoloFedeClient/src/progettopaolofede/Client.java
@@ -11,16 +11,12 @@ public class Client extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         BorderPane root = new BorderPane();
-        FXMLLoader listLoader = new FXMLLoader(getClass().getResource("list.fxml"));
-        root.setLeft(listLoader.load());
         FXMLLoader sendLoader = new FXMLLoader(getClass().getResource("send.fxml"));
         root.setRight(sendLoader.load());
         ClientController clientController = sendLoader.getController();
-        ListController listController = listLoader.getController();
         DataModel model = new DataModel();
         clientController.initModel(model);
         clientController.start();   // THREAD CHE SI METTE IN ATTESA DI RICEVERE MAIL DAL SERVER
-        listController.initModel(model);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
--- ProgettoPaoloFedeClient/src/progettopaolofede/ClientController.java
+++ ProgettoPaoloFedeClient/src/progettopaolofede/ClientController.java
@@ -18,77 +18,10 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-/* 
- public class ClientController implements Initializable {
-
- private List<String> listaMsg = new ArrayList<String>(); // lista msg
-
- ObservableList<String> observableList; // per listView
-
- @FXML
- private TextField clientDestTxtField;
- @FXML
- private TextField clientMsgTextField;
- @FXML
- private ListView listview;
-
- @FXML
- private void handleButtonAction(ActionEvent event) {
- System.out.println("You clicked me!");
- String messaggio = clientMsgTextField.getText();
-
- ArrayList<String> destinatario = null;
- destinatario.add(clientMsgTextField.getText());
-
- // Email email = new Email(0, "utente0", destinatario, "argomento", clientMsgTextField.getText(), "today"); //DESTINATARIO LISTA
- Email email = new Email(0, "utente0", "destinatario", "argomento", clientMsgTextField.getText(), "today");
- listaMsg.add(email.toString()); //aggiungo email ad arrayList
- observableList = FXCollections.observableList(listaMsg); //creo observableList e riempio con array di msg
- listview.setItems(observableList);//aggiungo oggetto a listViews GUI
-
- }
-
- @FXML
- private void writeMsg(ActionEvent event) {
- try {
- Socket s = new Socket("localhost", 5000); //localhost
- try {
- //               
- //                 InputStream inStream = s.getInputStream(); //flusso in uscita da server a client
- //                 OutputStream outStream = s.getOutputStream();//flusso in uscita da client a server
- //                 PrintWriter out = new PrintWriter(outStream, true); //oggetto per inviare oggetti a server da client..
- //                 Scanner in = new Scanner(inStream); //oggetto per prendere oggetti inviati da server a client
- //
- //                 out.println("client scrive a server");
- //                 
-
- Calendar cal = Calendar.getInstance(); //crea oggetto cal inizializzato all'ora e data corrente
- ArrayList<String> destinatario = new ArrayList<>();
- destinatario.add(clientMsgTextField.getText());
- Email email = new Email(0, "utente0", "destinatario", "argomento", clientMsgTextField.getText(), cal.getTime().toString());
- //           EmailHandler emailHandler= new EmailHandler(email,"WRITE");
- ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
- //                out.writeObject(emailHandler);
- out.close();
- } finally {
- s.close();
- }
- } catch (IOException e) {
- System.out.println("ERRORE");
- e.printStackTrace();
- }
- }
-
- @Override
- public void initialize(URL url, ResourceBundle rb) {
- // TODO
- }
-
- }
- */
 public class ClientController implements Initializable, Serializable {
 
     private DataModel model; //model del client
+
     private String Action; //rappresenta azione da eseguire
     @FXML
     private ListView<Email> listView;
@@ -128,36 +61,30 @@ public class ClientController implements Initializable, Serializable {
         this.model = model;
         listView.setItems(model.getEmailList());
     }
-        /* model.emailList.addListener((obs,oldEmail,newEmail)-> {
-         if(newEmail == null)
-         listView.setItems(null);
-         else
-         listView.setItems(newEmail);
-        
-         });
-         */
-    
-//comm
-    /*
+
      public void start() {
-     Thread clientThread = new Thread(() -> {
-     try {
-     ServerSocket s = new ServerSocket(5000);
+         Thread clientThread = new Thread(() -> {
+         try {
+             ServerSocket s = new ServerSocket(5000);
 
-     while(true) {
-     ObjectInputStream in = new ObjectInputStream(incoming.getInputStream());
-     EmailManager e = (EmailManager) in.readObject(); // UPCAST perchè so che riceverò ogg EmailManager
-     Email mail = e.getEmail();
+             while(true) {
+                 incoming = s.accept();
 
-     textArea2.setText(mail.getTesto());
+                 ObjectInputStream in = new ObjectInputStream(incoming.getInputStream());
+                 EmailManager e = (EmailManager) in.readObject(); // UPCAST perchè so che riceverò ogg EmailManager
+                 Email mail = e.getEmail();
+
+                 textArea2.setText(mail.getTesto());
+
+                 incoming.close();
+             }
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+         });
+         clientThread.start();
      }
-     } catch (Exception e) {
-     e.printStackTrace();
-     }
-     });
-     clientThread.start();
-     }
-     */
+
 
     @FXML
     private void modifyList(ActionEvent event) {
