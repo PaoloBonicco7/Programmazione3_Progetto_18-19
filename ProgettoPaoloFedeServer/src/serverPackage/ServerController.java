41a182452a41b95a7931c383036570153bc3f5c3
gniammo <gniammo@gniammo-pc>
2019-05-15 13:35:59 +0200

    commit

--- ProgettoPaoloFedeServer/src/serverPackage/ServerController.java
+++ ProgettoPaoloFedeServer/src/serverPackage/ServerController.java
@@ -77,9 +77,9 @@ public class ServerController implements Initializable {
 
                             textAreaMail.setText(email);
                             /*
-                             *   WRITE = il server quando riceve scrive su json e poi va informato il client destinatario del msg (observable?)
+                             *   SEND = il server quando riceve scrive su json e poi va informato il client destinatario del msg (observable?)
                              *   WRITEALL= direi che possiamo usare solo write e aggiornare piu' client, no?
-                             *   REMOVE = il server riceve l'email, la cercanel json e la rimuove
+                             *   REMOVE = il server riceve l'email, la cerca nel json e la rimuove
                              *   REPLY = viene creato un oggetto email copiandolo da quelli che vede il cliente e lo spedice al mittente , server fa come write
                              *   REPLY ALL = come reply ma a tutti
                              */
