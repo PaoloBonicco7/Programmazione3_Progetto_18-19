5a4d8e50a3a5558b396536d435d456705143d390
gniammo <gniammo@gniammo-pc>
2019-05-15 13:31:19 +0200

    implementato reply all implementato removeMsg. Compila ma non funziona in quanto durante l'esecuzione , in particolare quando viene inviato (client->Server)  l'oggetto emailManager contenente il msg da eliminare viene restituito l'errore "notSerializableObject".

--- ProgettoPaoloFedeClient/src/progettopaolofede/ClientController.java
+++ ProgettoPaoloFedeClient/src/progettopaolofede/ClientController.java
@@ -9,6 +9,8 @@ import java.util.Calendar;
 import java.util.ResourceBundle;
 import comunication.Email;
 import comunication.EmailManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -46,12 +48,13 @@ public class ClientController implements Initializable, Serializable {
     private Button deleteButton;
 
     Socket incoming = null;
+    private int serverSocket = 5000;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
     }
-    
+
     @FXML
     public void initModel(DataModel model) {
         if (this.model != null) {
@@ -62,29 +65,42 @@ public class ClientController implements Initializable, Serializable {
         listView.setItems(model.getEmailList());
     }
 
-     public void start() {
-         Thread clientThread = new Thread(() -> {
-         try {
-             ServerSocket s = new ServerSocket(5001);
+    public void start() {
+        Thread clientThread = new Thread(() -> {
+            try {
+                ServerSocket s = new ServerSocket(5001);
 
-             while(true) {
-                 incoming = s.accept();
+                while (true) {
+                    incoming = s.accept();
 
-                 ObjectInputStream in = new ObjectInputStream(incoming.getInputStream());
-                 EmailManager e = (EmailManager) in.readObject(); // UPCAST perchè so che riceverò ogg EmailManager
-                 Email mail = e.getEmail();
+                    ObjectInputStream in = new ObjectInputStream(incoming.getInputStream());
+                    EmailManager e = (EmailManager) in.readObject(); // UPCAST perchè so che riceverò ogg EmailManager
+                    Email mail = e.getEmail();
 
-                 textArea2.setText(mail.getTesto());
+                    textArea2.setText(mail.getTesto());
 
-                 incoming.close();
-             }
-         } catch (Exception e) {
+                    incoming.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        clientThread.start();
+    }
+
+    private Socket connect() throws IOException {
+        return new Socket("localhost", serverSocket);
+    }
+
+    private void disconnect(Socket s) {
+        try {
+            s.close();
+        } catch (IOException e) {
+
             e.printStackTrace();
-         }
-         });
-         clientThread.start();
-     }
+        }
 
+    }
 
     @FXML
     private void modifyList(ActionEvent event) {
@@ -130,24 +146,68 @@ public class ClientController implements Initializable, Serializable {
             e.printStackTrace();
         }
     }
-    
+
     @FXML
-    public void replyMsg(ActionEvent event) {
+    private void replyMsg(ActionEvent event) {
         Email email = listView.getSelectionModel().getSelectedItem();
         String argomento = email.getArgomento();
         String testo = email.getTesto();
-        String mittente ="TODOMIttente";
+        String mittente = "TODOMIttente";
         textFieldObject.setText(argomento);
         textFieldFrom.setText(mittente);
         textArea.setText(testo);
+
     }
 
-     @FXML
+    @FXML
     private void removeMsg(ActionEvent event) {
         Email email = listView.getSelectionModel().getSelectedItem();
         // listView.getItems().remove(item); //oggetto rimosso , solo da listview
         model.getEmailList().remove(email); //oggetto rimosso dal model->si propaga sulla listview
-        EmailManager emailManager = new EmailManager(email, "REMOVE");
-        //TODO -> inviare msg a server e fare eliminare a lui il msg dal json
+        
+        Calendar cal = Calendar.getInstance(); //crea oggetto cal inizializzato all'ora e data corrente
+        String id = "ID";
+        String mittente = email.getMittente();
+        ArrayList<String> destinatari = email.getDestinatario();
+        String object = email.getArgomento();
+        String text = email.getTesto();
+        String time = cal.getTime().toString();
+        Email e = new Email(id,mittente,destinatari,object,text,time);
+        EmailManager emailManager = new EmailManager(e,"REMOVE");
+
+        try {
+            Socket s = connect();
+            try {
+                ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+                out.writeObject(emailManager);
+                out.close();
+
+            } finally {
+                disconnect(s);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+
+        }
     }
+
+    @FXML
+    private void replyAllmsg() {
+        Email email = listView.getSelectionModel().getSelectedItem();
+        String elencoDestinatari = "";
+        String argomento = email.getArgomento();
+        String testo = email.getTesto();
+        String mittente = "TODOMIttente";
+        ArrayList<String> destinatari = new ArrayList<String>();
+        destinatari.add("destinatarioTODO"); //devo aggiungere tutti i destinatari qua dentro.
+        //todo
+        for (String a : destinatari) {
+            elencoDestinatari = elencoDestinatari + "," + a;
+        }
+        textFieldTo.setText(elencoDestinatari);//destinatari
+        textFieldObject.setText(argomento);//Argomento msg
+        textFieldFrom.setText(mittente);//mittente msg
+        textArea.setText(testo);//testo msg
+    }
+
 }
